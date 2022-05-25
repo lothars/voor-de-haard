f8544857b4c31fb14209bf51ff33dd43172d3de0
lothars <lothars@users.noreply.github.com>
2022-05-25 15:58:41 +0200

    rest_framework example

--- alchemist_cookbook/alchemist_cookbook/urls.py
+++ alchemist_cookbook/alchemist_cookbook/urls.py
@@ -14,8 +14,29 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.contrib.auth.models import User
+from django.urls import include, path
+from rest_framework import routers, serializers, viewsets
 
+#Serializers define API representation.
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'is_staff']
+        
+#ViewSets define the view behaviour
+class UserViewset(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    
+#routers provide an easy way of automatically determining the URL conf
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+
+#Wire up your API using automatic url routing.
+#Include login urls for the browsable API.
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
